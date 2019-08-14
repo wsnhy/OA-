1ad1bb2686c413e5c35e9e9de372e6eee1bb51af
zlb <zlb1045456368@163.com>
2019-08-14 16:50:45 +0800

    6

--- js/page.js/OA_list_one.js
+++ js/page.js/OA_list_one.js
@@ -5,169 +5,54 @@ layui.config({
     index: 'lib/index' //主入口模块
 }).use(['index', 'form', 'table'], function() {
     $ = layui.jquery
-
-
-
-
 });
 
 // 001
 var myChart = echarts.init(document.getElementById('project'));
-
-// option = {
-//   tooltip: {
-//     trigger: 'item',
-//     formatter: "{a} <br/>{b}: {c} ({d}%)"
-//   },
-//   legend: {
-//     orient: 'vertical',
-//     x: 'left',
-//     data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
-//   },
-//   series: [
-//     {
-//       name: '访问来源',
-//       type: 'pie',
-//       radius: ['50%', '70%'],
-//       avoidLabelOverlap: false,
-//       label: {
-//         normal: {
-//           show: false,
-//           position: 'center'
-//         },
-//         emphasis: {
-//           show: true,
-//           textStyle: {
-//             fontSize: '30',
-//             fontWeight: 'bold'
-//           }
-//         }
-//       },
-//       labelLine: {
-//         normal: {
-//           show: false
-//         }
-//       },
-//       data: [
-//         { value: 335, name: '直接访问' },
-//         { value: 310, name: '邮件营销' },
-//         { value: 234, name: '联盟广告' },
-//         { value: 135, name: '视频广告' },
-//         { value: 1548, name: '搜索引擎' }
-//       ]
-//     }
-//   ]
-// }
-option = {
-    tooltip: {
-        trigger: 'item',
-        formatter: "{a} <br/>{b}: {c} ({d}%)"
+kyoption = {
+    title: {
+        text: '我的科研统计',
     },
-    legend: {
-        orient: 'vertical',
-        x: 'left',
-        data: ['直达', '营销广告', '搜索引擎', '邮件营销', '联盟广告', '视频广告', '百度', '谷歌', '必应', '其他']
+    xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    tooltip: {
+        axisPointer: { // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+        }
+    },
+    // grid: {
+    //     left: '3%',
+    //     right: '4%',
+    //     bottom: '3%',
+    //     containLabel: true
+    // },
+    toolbox: {
+        show: true,
+        feature: {
+            dataZoom: {
+                yAxisIndex: 'none'
+            },
+            dataView: { readOnly: false },
+            magicType: { type: ['line', 'bar'] },
+            restore: {},
+            saveAsImage: {}
+        }
     },
     series: [{
-            name: '访问来源',
-            type: 'pie',
-            selectedMode: 'single',
-            radius: [0, '30%'],
-
-            label: {
-                normal: {
-                    position: 'inner'
-                }
-            },
-            labelLine: {
-                normal: {
-                    show: false
-                }
-            },
-
+        type: 'bar',
+        label: {
+            normal: {
+                show: true,
+                position: 'top'
+            }
         },
-        {
-            name: '访问来源',
-            type: 'pie',
-            radius: ['40%', '55%'],
-            label: {
-                normal: {
-                    formatter: '{a|{a}}{abg|}\n{hr|}\n  {b|{b}：}{c}  {per|{d}%}  ',
-                    backgroundColor: '#eee',
-                    borderColor: '#aaa',
-                    borderWidth: 1,
-                    borderRadius: 4,
-                    // shadowBlur:3,
-                    // shadowOffsetX: 2,
-                    // shadowOffsetY: 2,
-                    // shadowColor: '#999',
-                    // padding: [0, 7],
-                    rich: {
-                        a: {
-                            color: '#999',
-                            lineHeight: 22,
-                            align: 'center'
-                        },
-                        // abg: {
-                        //     backgroundColor: '#333',
-                        //     width: '100%',
-                        //     align: 'right',
-                        //     height: 22,
-                        //     borderRadius: [4, 4, 0, 0]
-                        // },
-                        hr: {
-                            borderColor: '#aaa',
-                            width: '100%',
-                            borderWidth: 0.5,
-                            height: 0
-                        },
-                        b: {
-                            fontSize: 16,
-                            lineHeight: 33
-                        },
-                        per: {
-                            color: '#eee',
-                            backgroundColor: '#334455',
-                            padding: [2, 4],
-                            borderRadius: 2
-                        }
-                    }
-                }
-            },
-            data: [{
-                    value: 335,
-                    name: '直达'
-                },
-                {
-                    value: 310,
-                    name: '邮件营销'
-                },
-                {
-                    value: 234,
-                    name: '联盟广告'
-                },
-                {
-                    value: 135,
-                    name: '视频广告'
-                },
-                {
-                    value: 1048,
-                    name: '百度'
-                },
-                {
-                    value: 251,
-                    name: '谷歌'
-                },
-                {
-                    value: 147,
-                    name: '必应'
-                },
-                {
-                    value: 102,
-                    name: '其他'
-                }
-            ]
-        }
-    ]
+        data: [120, 200, 150, 80, 70, 110, 130]
+
+    }]
 };
-myChart.setOption(option);
+myChart.setOption(kyoption);
